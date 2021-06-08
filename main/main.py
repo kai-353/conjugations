afceb0127fd4c133f1ca180635506b3a0ecb907f
kai-353 <70913300+kai-353@users.noreply.github.com>
2021-06-08 17:02:49 +0200

    Add files via upload

---
+++ main/main.py
@@ -0,0 +1,31 @@
+
+from vervoeger import Vervoeger
+
+option_dict = {
+    "0": "all",
+    "1": "Indicativo presente",
+    "2": "Indefinido",
+    "3": "Imperfecto",
+    "4": "Pretérito perfecto compuesto",
+    "5": "Pluscuamperfecto",
+    "6": "Futuro I",
+    "7": "Futuro perfecto"
+}
+
+
+def print_dict(dictionary):
+    for key in dictionary:
+        print(key, dictionary[key])
+
+
+if __name__ == '__main__':
+    vervoeger = Vervoeger()
+
+    print_dict(option_dict)
+    while True:
+        werkwoord = input('werkwoord: ')
+        optie = input('nummer: ')
+        if optie == '0':
+            vervoeger.get_conjugations(werkwoord)
+        else:
+            vervoeger.get_conjugation(werkwoord, option_dict[optie])
---
+++ main/vervoeger.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+def conjugation_error(werkwoord):
+    print(f"Sorry, er zijn geen (Spaanse) vervoegingen gevonden van '{werkwoord}'")
+
+
+class Vervoeger:
+
+    def __init__(self):
+        self.PATH = "C:\Program Files (x86)\chromedriver.exe"
+
+    def get_conjugation(self, werkwoord, optie):
+        driver = webdriver.Chrome(self.PATH)
+        try:
+            driver.get(f"https://www.vertalen.nu/vervoeging?vervoeg={werkwoord}&taal=es")
+            header = driver.find_element_by_xpath(f"//*[contains(text(), '{optie}')]")
+            parent = header.find_element_by_xpath("./..")
+            # conjugation_list = parent.find_element_by_tag_name("ul")
+            print(parent.text)
+            driver.close()
+        except NoSuchElementException:
+            driver.close()
+            conjugation_error(werkwoord)
+
+    def get_conjugations(self, werkwoord):
+        driver = webdriver.Chrome(self.PATH)
+        try:
+            driver.get(f"https://www.vertalen.nu/vervoeging?vervoeg={werkwoord}&taal=es")
+            ul = driver.find_element_by_class_name("conjugation-container")
+            print(ul.text)
+            driver.close()
+        except NoSuchElementException:
+            driver.close()
+            conjugation_error(werkwoord)
